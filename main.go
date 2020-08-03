fbec0b40522a6a084709528e23cb9fe1e86ea1a3
邱阿朋 <qiuapeng@vchangyi.com>
2020-08-03 11:08:47 +0800

    update

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/", func(context *gin.Context) {
-		context.String(http.StatusOK, "我艹无情,好残忍++1")
+		context.String(http.StatusOK, "hello world")
 		return
 	})
 	err := r.Run("0.0.0.0:8080")
